9f76253b7f6249f944f2582076a225e444bbf8ce
Simon Wiles <simonjwiles@gmail.com>
2020-10-21 15:58:09 -0700

    Allow environment to override DB connection settings
    Password remains optional.

--- v2/conn.php
+++ v2/conn.php
@@ -1,3 +1,10 @@
 <?php
-$connection_string = "host=orbis_db dbname=orbis user=webapp";
+$dbhost = getenv("DBHOST") ?: "localhost";
+$dbname = getenv("DBNAME") ?: "orbis";
+$dbuser = getenv("DBUSER") ?: "webapp";
+
+$connection_string = "host={$dbhost} dbname={$dbname} user={$dbuser}";
+
+if (getenv("DBPASSWORD")) $connection_string .= " password={getenv('DBPASSWORD')}";
+
 ?>
